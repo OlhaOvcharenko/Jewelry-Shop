14699182c19c972433d46e30e269550a731f4e1f
Olha <ovcharenko.olga.2014@gmail.com>
2024-03-06 22:26:33 +0300

    add cors to app.module, create prisma.module and change main.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
-import { Module } from '@nestjs/common';
+import {
+  Module,
+  NestModule,
+  MiddlewareConsumer,
+  RequestMethod,
+} from '@nestjs/common';
+import * as cors from 'cors';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './config/configuration';
+import { PrismaModule } from './prisma/prisma.module';
+
 
 @Module({
-  imports: [],
+  imports: [ PrismaModule, ConfigModule.forRoot({ load: [configuration], isGlobal: true }) ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): void {
+    consumer.apply(cors()).forRoutes({
+      path: '*',
+      method: RequestMethod.ALL,
+    });
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.setGlobalPrefix('api');
+ 
+  await app.enableShutdownHooks();
+  await app.listen(configService.get('port'));
 }
 bootstrap();
