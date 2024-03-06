c161e20d213394923573c8e113c8636ffa87b49e
Olha <ovcharenko.olga.2014@gmail.com>
2024-03-06 23:09:39 +0300

    add endpoint get all products

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,14 @@
 import { Controller } from '@nestjs/common';
+import { ProductsService } from './products.service';
+import { Get } from '@nestjs/common';
+
 
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+  constructor(private productsService: ProductsService) {}
+
+  @Get('/')
+  async getAll() {
+    return this.productsService.getAll();
+  }
+}
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { Product } from '@prisma/client';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+   constructor(private prismaService: PrismaService) {}
+
+   public getAll(): Promise<Product[]> {
+    return this.prismaService.product.findMany();
+  }
+}
