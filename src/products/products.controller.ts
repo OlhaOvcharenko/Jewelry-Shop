82e71e6dc93f70c10f5fe026a7e472e68f4cb358
Olha <ovcharenko.olga.2014@gmail.com>
2024-03-09 16:15:58 +0300

    create endpoints to get all orders and get one order

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,14 +1,25 @@
 import { Controller } from '@nestjs/common';
 import { ProductsService } from './products.service';
-import { Get } from '@nestjs/common';
+import { Get, Post, Body } from '@nestjs/common';
+import { NotFoundException, BadRequestException } from '@nestjs/common';
+import { Param } from '@nestjs/common';
+import { ParseUUIDPipe } from '@nestjs/common';
 
 
-@Controller('products')
+
+@Controller()
 export class ProductsController {
   constructor(private productsService: ProductsService) {}
 
-  @Get('/')
+  @Get('/products')
   async getAll() {
     return this.productsService.getAll();
   }
+  @Get('products/:id')
+  async getById(@Param('id', new ParseUUIDPipe()) id: string) {
+    const prod = await this.productsService.getById(id);
+    if (!prod) throw new NotFoundException('Product not found');
+    return prod;
+  }
+
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,12 +1,58 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { Product } from '@prisma/client';
+import { Order } from '@prisma/client';
 
 @Injectable()
 export class ProductsService {
    constructor(private prismaService: PrismaService) {}
 
-   public getAll(): Promise<Product[]> {
+  public getAll(): Promise<Product[]> {
     return this.prismaService.product.findMany();
   }
+
+  public getById(id: Product['id']): Promise<Product | null> {
+    return this.prismaService.product.findUnique({
+      where: { id },
+    });
+  }
+
+
+  public async addToOrder(productId: string, orderId: string): Promise <Order | null> {
+    try {
+      // Retrieve the product based on productId
+      const product = await this.prismaService.product.findUnique({
+        where: { id: productId },
+      });
+
+      if (!product) {
+        throw new Error('Product not found');
+      }
+
+      // Retrieve the order based on orderId
+      let order = await this.prismaService.order.findUnique({
+        where: { id: orderId },
+      });
+
+      if (!order) {
+        throw new Error('Order not found');
+      }
+
+      // Add the product to the order
+      order = await this.prismaService.order.update({
+        where: { id: orderId },
+        data: {
+          products: {
+            create: [product],
+          },
+        },
+      });
+
+      return order;
+    } catch (error) {
+      console.error('Error adding product to order:', error);
+      throw new Error('Failed to add product to order');
+    }
+  }
+
 }
