8912004774c869909d94f25c5213194fa980b388
Olha <ovcharenko.olga.2014@gmail.com>
2024-03-09 15:47:56 +0300

    change seed.ts and schema

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,53 +2,53 @@ import { PrismaClient } from '@prisma/client';
 const db = new PrismaClient();
 
 function getProducts() {
-    return [
-      {
-        id: 'fd105551-0f0d-4a9f-bc41-c559c8a17256',
-        name: 'Diamond Ring',
-        price: 2000,
-        description: 'Elegant diamond ring, perfect for special occasions',
-        collection: 'Jewelry',
-        photo: 'images/diamond-ring.jpg',
-        quantity: 1
-      },
-      {
-        id: 'c920c7b9-a67d-4edb-8ce7-e3c9f3889e56',
-        name: 'Pearl Necklace',
-        price: 5000,
-        description: 'Exquisite pearl necklace, timeless beauty',
-        collection: 'Jewelry',
-        photo: 'mages/pereal-necklace.jpg',
-        quantity: 1
-      },
-      {
-        id: 'fd105551-0f0d-4a9f-bc41-c559c8a17258',
-        name: 'Sapphire Earrings',
-        price: 3000,
-        description: 'Stunning sapphire earrings, adds a pop of color to any outfit',
-        collection: 'Jewelry',
-        photo: 'lmages/sapphire-earrings.jpg',
-        quantity: 0
-      },
-      {
-        id: 'fd105551-0f0d-4a9f-bc41-c559c8a17259',
-        name: 'Gold Bracelet',
-        price: 2000,
-        description: 'Luxurious gold bracelet, a timeless classic',
-        collection: 'Jewelry',
-        photo: 'mages/gold-bracelet.jpg',
-        quantity: 0
-      },
-      {
-        id: '01c7599d-318b-4b9f-baf7-51f3a936a2d4',
-        name: 'Ruby Pendant',
-        price: 5000,
-        description: 'Exquisite ruby pendant, adds elegance and charm',
-        collection: 'Jewelry',
-        photo: 'mages/ruby-pendant.jpg',
-        quantity: 0
-      },
-    ];
+  return [
+    {
+      id: 'fd105551-0f0d-4a9f-bc41-c559c8a17256',
+      name: 'Diamond Ring',
+      price: 2000,
+      description: 'Elegant diamond ring, perfect for special occasions',
+      collection: 'Jewelry',
+      photo: 'images/diamond-ring.jpg',
+      quantity: 0
+    },
+    {
+      id: 'c920c7b9-a67d-4edb-8ce7-e3c9f3889e56',
+      name: 'Pearl Necklace',
+      price: 5000,
+      description: 'Exquisite pearl necklace, timeless beauty',
+      collection: 'Jewelry',
+      photo: 'images/pearl-necklace.jpg',
+      quantity: 0
+    },
+    {
+      id: 'fd105551-0f0d-4a9f-bc41-c559c8a17258',
+      name: 'Sapphire Earrings',
+      price: 3000,
+      description: 'Stunning sapphire earrings, adds a pop of color to any outfit',
+      collection: 'Jewelry',
+      photo: 'images/sapphire-earrings.jpg',
+      quantity: 0
+    },
+    {
+      id: 'fd105551-0f0d-4a9f-bc41-c559c8a17259',
+      name: 'Gold Bracelet',
+      price: 2000,
+      description: 'Luxurious gold bracelet, a timeless classic',
+      collection: 'Jewelry',
+      photo: 'images/gold-bracelet.jpg',
+      quantity: 0
+    },
+    {
+      id: '01c7599d-318b-4b9f-baf7-51f3a936a2d4',
+      name: 'Ruby Pendant',
+      price: 5000,
+      description: 'Exquisite ruby pendant, adds elegance and charm',
+      collection: 'Jewelry',
+      photo: 'images/ruby-pendant.jpg',
+      quantity: 0
+    },
+  ];
 }
 
 async function seed() {
