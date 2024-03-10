f0f23a250fe865447908dc4c1d61bdf141d233de
Olha <ovcharenko.olga.2014@gmail.com>
2024-03-10 13:40:47 +0300

    change app.js and index.js by adding routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/pages/Home/Home';
+import NotFound from './components/pages/Not Found/NotFound';
+;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+const App = () => {
+  return ( 
+    <main>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes> 
+    </main>
   );
-}
+};
 
 export default App;
+
