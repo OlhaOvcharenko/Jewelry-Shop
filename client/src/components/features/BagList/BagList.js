9b07b57d26a681f0dd1ce8d7a793fec40aab85aa
Olha <ovcharenko.olga.2014@gmail.com>
2024-03-22 14:18:40 +0300

    change OrderForm

--- client/src/components/features/BagList/BagList.js
+++ client/src/components/features/BagList/BagList.js
@@ -1,7 +1,7 @@
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Button } from "react-bootstrap";
 import BagItem from "../BagItem/BagItem";
 import PageContainer from "../../common/PageContainer/PageContainer";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { getAllBagProducts } from "../../../redux/bagRedux";
 import { useEffect } from "react";
@@ -11,17 +11,28 @@ import { removeFromBag } from "../../../redux/bagRedux";
 
 const BagList = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate(); // Initialize useHistory
   const [bagProducts, setBagItems] = useState([]);
+  const [totalAmount, setTotalAmount] = useState(0); // State to hold the total amount
 
   const storedBagItems = useSelector(state => getAllBagProducts(state));
 
   useEffect(() => {
     setBagItems(storedBagItems);
+
+    // Calculate total amount
+    const amount = storedBagItems.reduce((total, item) => total + item.subTotal, 0);
+    setTotalAmount(amount);
   }, [storedBagItems]);
 
   const handleRemoveItem = (id) => {
     dispatch(removeFromBag({ id }));
   };
+
+  const handleOrderSubmit = () => {
+    navigate('/order', { state: { totalAmount } });
+  };
+
 
   return (
     <Container>
@@ -30,11 +41,19 @@ const BagList = () => {
           <BagItem key={product.id} bagProduct={product} onRemove={handleRemoveItem} />
         ))}
       </PageContainer>
-      <Row>
-        <Col className="text-end mt-3">
-          <Link to="/order" className="btn btn-secondary">Submit</Link> 
+
+      <Row className="mt-4">
+        <Col className="text-end">
+          <h4>Total Amount: {totalAmount} zl</h4>
         </Col>
       </Row>
+      
+      <Row>
+        <Col className="text-end mt-3">
+          <Button onClick={handleOrderSubmit} className="btn btn-secondary px-5 my-4 mx-3" size="lg">Submit</Button> 
+        </Col>
+      </Row>
+  
     </Container>
   );
 };
--- client/src/components/features/OrderForm/OrderForm.js
+++ client/src/components/features/OrderForm/OrderForm.js
@@ -1,31 +1,74 @@
-import { Form, Row, Col } from "react-bootstrap";
+import { Form, Row, Col, Button, Card} from "react-bootstrap";
 import OrderItems from "../OrderItems/OrderItems";
 import PageContainer from "../../common/PageContainer/PageContainer";
-
+import { useLocation } from "react-router-dom";
 
 const OrderForm = () => {
-  return(
-  <PageContainer>
-    <Row>
-      <Col md={6}>
-        <OrderItems />
-      </Col>
-      <Col md={6}>
-      <Form>
-        <Form.Group className="mb-3" >
-          <Form.Control placeholder="First name" />
-        </Form.Group>
-        <Form.Group className="mb-3">
-          <Form.Control placeholder="Last name" />
-        </Form.Group>
-        <Form.Group className="mb-3">
-          <Form.Control placeholder="Email name" />
-        </Form.Group>
-        </Form>
-      </Col>
-    </Row>
-   </PageContainer>
+  const location = useLocation();
+  const totalAmount = location.state.totalAmount;
+
+  return (
+    <PageContainer>
+       <h1 className="text-center mb-4">Order</h1>
+      <Row>
+        <Col md={6} >
+        
+          <OrderItems />
+          
+        </Col>
+
+        <Col md={6}>
+          <Card>
+            <Card.Body>
+              <Form>
+                <Row className="mb-3">
+                  <Form.Group as={Col} controlId="formGridName">
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control type="text" placeholder="Name" />
+                  </Form.Group>
+
+                  <Form.Group as={Col} controlId="formGridSurname">
+                    <Form.Label>Surname</Form.Label>
+                    <Form.Control type="text" placeholder="Surname" />
+                  </Form.Group>
+                </Row>
+
+                <Row className="mb-3">
+                  <Form.Group as={Col} controlId="formGridEmail">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control type="email" placeholder="Email" />
+                  </Form.Group>
+
+                  <Form.Group as={Col} controlId="formGridNumber">
+                    <Form.Label>Phone number</Form.Label>
+                    <Form.Control type="text" placeholder="Phone" />
+                  </Form.Group>
+                </Row>
+
+                <Form.Group className="mb-3" controlId="formGridAddress">
+                  <Form.Label>Address </Form.Label>
+                  <Form.Control placeholder="Country, city, postal code, street, apartment" />
+                </Form.Group>
+              </Form>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+
+      <Row className="mt-4">
+        <Col className="text-end">
+          <h4>Total Amount: {totalAmount} zl</h4>
+        </Col>
+      </Row>
+
+      <Row>
+        <Col className="text-end mt-3">
+          <Button className="btn btn-secondary px-5 my-4 mx-3" size="lg">Create</Button>
+        </Col>
+      </Row>
+    </PageContainer>
   )
 }
+
 
 export default OrderForm;
