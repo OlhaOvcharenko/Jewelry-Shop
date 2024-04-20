fc2c5d60709166aaa367678260cd4cea757dde2a
Olha <ovcharenko.olga.2014@gmail.com>
2024-04-20 21:45:38 +0300

    change user redux and login for by adding new action and states for loggining

--- client/src/components/features/LogInForm/LogInForm.js
+++ client/src/components/features/LogInForm/LogInForm.js
@@ -1,10 +1,51 @@
 import { Col, Container } from "react-bootstrap";
 import styles from "../LogInForm/LogInForm.module.scss"
-import { Form, Row} from "react-bootstrap";
+import { Form, Alert, Spinner} from "react-bootstrap";
 import Button from "../../common/Button/Button";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { API_URL } from "../../../config";
+import { logIn } from "../../../redux/usersRedux";
 
-const LogInForm = () => (
+const LogInForm = () => {
+
+  const dispatch = useDispatch();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [status, setStatus] = useState(null)
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const options = {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json' 
+      },
+      body: JSON.stringify({ email, password })
+    }
+
+    setStatus("loading");
+    fetch(`${API_URL}/api/auth/login`, options)
+      .then((res) => {
+        if (res.status === 201) {
+          setStatus("success");
+          dispatch(logIn({email}));
+        } else if (res.status === 400) {
+          setStatus("clientError");
+        } else if (res.status === 409) {
+          setStatus("loginError");
+        } else {
+          setStatus("serverError");
+        }
+      })
+      .catch((err) => {
+        setStatus("serverError");
+      });
+  }
+
+  return (
     <div>
       <div className={styles.accountHeader}>
         <h1 className={styles.accountTitle}>ACCOUNT</h1>
@@ -13,28 +54,66 @@ const LogInForm = () => (
       </div>
       <Container>
         <div className={styles.loginBox}>
-          
-          <Col lg={6} >
-            <Form className={styles.border}>
-              <Form.Group className="mb-3">
-                <Form.Label  className="text-muted">Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-              </Form.Group>
+          {(status === "success" || status === "serverError" || status === "clientError" || status === "loginError" || status === "loading") ? (
+            <>
+              {status === "success" && (
+                <Alert variant="success" className="text-center">
+                  <Alert.Heading>Success!</Alert.Heading>
+                  <p>Your account has been created.</p>
+                  <Link to="/" className={styles.headerLink}><b>Continue shopping</b></Link> 
+                </Alert>
+              )}
+              {status === "serverError" && (
+                <Alert variant="danger">
+                  <Alert.Heading>Something went wrong...</Alert.Heading>
+                  <p>Unexpected error, try again!</p>
+                  <Link to="/login" className={styles.headerLink}><b>Back to login</b></Link>
+                </Alert>
+              )}
+              {status === "clientError" && (
+                <Alert variant="danger">
+                  <Alert.Heading>Not enough data</Alert.Heading>
+                  <p>You have to fill all the fields</p>
+                  <Link to="/register" className={styles.headerLink}><b>Back to registration</b></Link>
+                </Alert>
+              )}
+              {status === "loginError" && (
+                <Alert variant="warning">
+                  <Alert.Heading>Login already in use</Alert.Heading>
+                  <p>You have to use another login</p>
+                  <Link to="/register" className={styles.headerLink}><b>Back to registration</b></Link>
+                </Alert>
+              )}
+              {status === "loading" && (
+                <Spinner animation="border" role="status">
+                  <span className="visually-hidden">Loading...</span>
+                </Spinner>
+              )}
+            </>
+          ) : (
+            <Col lg={6}>
+              <Form className={styles.border} onSubmit={handleSubmit}>
+                <Form.Group className="mb-3">
+                  <Form.Label className="text-muted">Email address</Form.Label>
+                  <Form.Control type="email" placeholder="Enter email" value={email} onChange={e => setEmail(e.target.value)} />
+                </Form.Group>
 
-              <Form.Group className="mb-3">
-                <Form.Label  className="text-muted">Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
-              </Form.Group>
-              <Button className={styles.btn}>
-                Login
-              </Button>
-            </Form>
-            <Link to="/register" className={styles.link}><b>Create account</b></Link>
-            <Link to="/products" className={styles.link}><b>Return to store</b></Link>
-          </Col>
+                <Form.Group className="mb-3">
+                  <Form.Label className="text-muted">Password</Form.Label>
+                  <Form.Control type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
+                </Form.Group>
+                <Button className={styles.btn} type="submit">
+                  Login
+                </Button>
+              </Form>
+              <Link to="/register" className={styles.link}><b>Create account</b></Link> /
+              <Link to="/products" className={styles.link}><b>Return to store</b></Link>
+            </Col>
+          )}
         </div>
       </Container>
     </div>
-);
+  )
+};
   
 export default LogInForm;
--- client/src/redux/usersRedux.js
+++ client/src/redux/usersRedux.js
@@ -4,12 +4,26 @@ import initialState from "./initialState";
 /*SELECTORS*/
 
 /*ACTIONS*/
+const createActionName = (actionName) => `app/users/${actionName}`;
+const LOG_IN = createActionName("LOG_IN");
+
 
 /*ACTION CREATORS*/
+
+export const logIn = (payload) => ({
+  type: LOG_IN,
+  payload,
+});
+
 
 /* REDUCER */
 export default function usersReducer(statePart = initialState, action = {}) {
   switch (action.type) {
+    case LOG_IN:
+      return {
+        ...statePart,
+        user: action.payload,
+      };
     default:
     return statePart;
   }
