92028429313349bbbe0a27d2e700a94dbb4ee3d4
Olha <ovcharenko.olga.2014@gmail.com>
2024-05-01 18:37:31 +0300

    build version

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { User } from '@prisma/client';
-import  { Password } from '@prisma/client';
+import { User, Password } from '@prisma/client';
 import { ConflictException } from '@nestjs/common';
 
 
