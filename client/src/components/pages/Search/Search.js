9fd3cb177b2f58a655318e72014d1ecbbd2aadd1
Olha <ovcharenko.olga.2014@gmail.com>
2024-06-08 14:40:29 +0300

    create new component search form

--- client/src/components/pages/Search/Search.js
+++ client/src/components/pages/Search/Search.js
@@ -1,58 +1,12 @@
-import PageContainer from "../../common/PageContainer/PageContainer";
-import { Form, Col, Row }from 'react-bootstrap';
-import Button from "../../common/Button/Button";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
-import { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { getAllProducts, getSearchedProducts, loadSearchRequest } from "../../../redux/productsRedux";
-import CardProduct from "../../features/CardProduct/CardProduct";
+import {Container }from 'react-bootstrap';
+import SearchForm from "../../features/SearchFrom/SearchForm";
 
 
 const Search = () => {
-  const searchedProducts = useSelector(getSearchedProducts);
-  console.log(searchedProducts);
-
-  const dispatch = useDispatch();
-  const [searchPhrase, setSearchPhrase] = useState('');
-  
-
-  const handleSearch = (e) => {
-    e.preventDefault();
-   dispatch(loadSearchRequest(searchPhrase))
-  }
-
   return (
-    <PageContainer>
-      <Form>
-        <Row className="justify-content-center text-center">
-          <Col lg={6}>
-            <Form.Group className="mb-3">
-              <h1 className="mb-3">Search</h1>
-              <Form.Group className="d-flex">
-                <Form.Control size="lg" type="text" placeholder="Search products" className="mx-3"
-                value={searchPhrase} onChange={e => setSearchPhrase(e.target.value)}/>
-                <Button onClick={handleSearch}>
-                  <FontAwesomeIcon icon={faSearch} />
-                </Button>
-              </Form.Group>
-            </Form.Group>
-          </Col>
-          
-          {searchedProducts.length > 0  && (
-          <Row xs={1} md={2} lg={4} className="d-flex justify-content-center text">
-            
-            {searchedProducts.map(product => (
-            <CardProduct key={product.id} product={product}/>  
-            ))}
-            
-          </Row>
-          )}
-        </Row>
-      </Form>
-
-      
-    </PageContainer>
+   <Container>
+      <SearchForm />
+   </Container>
   )
 };
   
