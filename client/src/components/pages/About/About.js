8f485a49bd01c6f4f6abc23dba513100f9adfea6
Olha <ovcharenko.olga.2014@gmail.com>
2024-07-17 20:43:00 +0300

    change About.js adding title, paragraph and img

--- client/src/components/pages/About/About.js
+++ client/src/components/pages/About/About.js
@@ -1,3 +1,4 @@
+import { Container } from "react-bootstrap";
 import BreadCrambSection from "../../common/Breadcramb/Breadcramb";
 import PageContainer from "../../common/PageContainer/PageContainer";
 
@@ -5,7 +6,24 @@ const About = () => {
   return(
     <div>
       <BreadCrambSection title='ABOUT' link='Home' />
-      <h1 className=" pt-3 text-center">TRENDY JEWELERY</h1>
+      <h1 className=" pt-4 text-center">TRENDY JEWELERY</h1>
+      <p className=" pt-3 text-center">
+        Suspendisse in ipsum sollicitudin, blandit purus id, blandit diam.
+      </p>
+      <p className=" pt-1 text-center">
+        Vivamus vulputate tincidunt elit aliquam malesuada. Quisque vel fringilla sapien.
+      </p>
+      <p className=" p-1 text-center">
+        Ut sollicitudin hendrerit eros et sodales. Donec
+        Mauris cursus mattis molestie a iac.
+      </p>
+      <Container>
+        <img 
+          src="https://images.pexels.com/photos/3474504/pexels-photo-3474504.jpeg" 
+          alt="Trendy Jewelry" 
+          style={{ maxWidth: '100%', height: '500px', display: 'block', marginLeft: 'auto', marginRight: 'auto' }} 
+        />
+      </Container>
     </div>
   )
 };
